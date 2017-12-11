a257ee3d88f9057e09431b3e819746a010736836
Stalf <kssgel@yandex.ru>
2017-12-11 21:41:45 +0500

    Initial commit

---
+++ client/src/main/java/ru/kss/chat/client/ClientMain.java
@@ -0,0 +1,13 @@
+package ru.kss.chat.client;
+
+import lombok.extern.slf4j.Slf4j;
+import ru.kss.chat.Utils;
+
+@Slf4j
+public class ClientMain {
+
+    public static void main(String... args) {
+        log.info(Utils.testMessage());
+    }
+
+}
---
+++ core/src/main/java/ru/kss/chat/Utils.java
@@ -0,0 +1,9 @@
+package ru.kss.chat;
+
+public class Utils {
+
+    public static String testMessage() {
+        return "chat testMessage";
+    }
+
+}
